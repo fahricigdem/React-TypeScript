ac93af65a3842d5c082621cf3c5a9a86d5616805
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-11-21 15:28:44 +0100

    react hook form start

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 //import App from "./ReactTypeScript-3/App";
 //import App from "./ReactTypeScript-4/App";
 //import { App } from "./ReactTypeScript-5/App";
-import { App } from "./App";
-import { App1 } from "./App1";
+//import { App } from "./App";
+//import { App1 } from "./App1";
+import { App } from "./react-hook-form/App";
 
 ReactDOM.render(
   <ChakraProvider>
-    <App1 />
+    <App />
   </ChakraProvider>,
   document.getElementById("root")
 );
---
+++ src/react-hook-form/App.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@chakra-ui/layout";
+
+export const App = () => {
+  return <Box p="32">react-hook-form</Box>;
+};
