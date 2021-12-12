32fd5e664c65866ffb3bd8126a50ef8539538e7f
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-12-12 04:49:13 +0100

    combobox

--- src/SelectwithObject/AppCombo.tsx
+++ src/SelectwithObject/AppCombo.tsx
@@ -14,15 +14,14 @@ let turbines = [
   { name: "Vestas", id: "2222", model: "BBB" },
   { name: "Siemens", id: "3333", model: "XXX" },
   { name: "Siemens", id: "4444", model: "YYY" },
-  { name: "aaaaa", id: "5555", model: "qqq" },
-  { name: "aaaaa", id: "6666", model: "lll" },
+  { name: "aaaaas", id: "5555", model: "qqq" },
+  { name: "aaaaas", id: "6666", model: "lll" },
 ];
 
 console.log("turbines", turbines);
 export const App = () => {
   const [myName, setMyName] = useState("Name");
   const [myId, setMyId] = useState("Id");
-  const names = Array.from(new Set(["Vestas", "Siemens"]));
 
   const [term, setTerm] = useState("");
 
@@ -43,8 +42,10 @@ export const App = () => {
   };
 
   let results = turbines
-    .filter((t) => t.name.toLowerCase().includes(term))
+    .filter((t) => `${t?.name} ${t?.model}`.toLowerCase().includes(term))
     .sort(compareFirstNames);
+
+  const names = Array.from(new Set(results.map((t) => t.name)));
 
   console.log("results", results);
   console.log("myName, myId:", myName, myId);
@@ -54,35 +55,69 @@ export const App = () => {
       <Combobox>
         <ComboboxInput
           onChange={handleChange}
-          style={{ width: 300, margin: 0 }}
+          style={{
+            width: 300,
+            margin: 0,
+            backgroundColor: "#ddd",
+            border: "solid 1px black",
+          }}
         />
         {results && (
           <ComboboxPopover
             onChange={(e) => console.log(e.target)}
-            style={{ width: 300, maxHeight: "120px", overflowY: "auto" }}
+            style={{
+              width: 300,
+              // maxHeight: "120px",
+              // overflowY: "auto"
+            }}
           >
             {results.length > 0 ? (
               <ComboboxList>
-                {results?.slice(0, 10).map((result, index) => {
-                  return (
-                    <ComboboxOption
-                      key={index}
-                      value={`${result?.name} ${result?.model}`}
-                      onClick={() =>
-                        console.log(
-                          "----->",
-                          `${result?.name} ${result?.model}`
-                        )
-                      }
-                    />
-                  );
-                })}
+                {results
+                  ?.map((turbine, index) => {
+                    if (names.includes(turbine.name)) {
+                      names.shift();
+                      return (
+                        <div key={index}>
+                          <ComboboxOption
+                            style={{
+                              fontWeight: "bold",
+                              backgroundColor: "#ddf",
+                            }}
+                            value={turbine.name}
+                            onClick={() => {
+                              setMyId("_");
+                              setMyName(turbine.name);
+                            }}
+                          />
+                          <ComboboxOption
+                            key={index}
+                            value={`${turbine?.name} ${turbine?.model}`}
+                            onClick={() => {
+                              setMyId(turbine.id);
+                              setMyName("_");
+                            }}
+                          />
+                        </div>
+                      );
+                    } else {
+                      return (
+                        <ComboboxOption
+                          key={index}
+                          value={`${turbine?.name} ${turbine?.model}`}
+                          onClick={() => {
+                            setMyId(turbine.id);
+                            setMyName("_");
+                          }}
+                        />
+                      );
+                    }
+                  })
+                  .slice(0, 4)}
               </ComboboxList>
             ) : (
               <div>
-                <p style={{ padding: 10, textAlign: "center" }}>
-                  No results 😞
-                </p>
+                <p style={{ padding: 10, textAlign: "center" }}>No results</p>
               </div>
             )}
           </ComboboxPopover>
